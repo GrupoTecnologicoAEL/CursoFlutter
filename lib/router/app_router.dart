6e7094a53c133a4a97ecfadf643c51d3222d49f2
LuisJuarez17 <ljuarezn@miumg.edu.gt>
2024-08-28 22:33:15 -0600

    Flaying (signup)
    Flaying flaying

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -9,30 +9,30 @@ import '../Screens/Admin/admin_screen.dart';
 import '../Screens/Admin/crud_product.dart';
 import '../Screens/Client/client_screen.dart';
 import '../Screens/Client/product_list_screen.dart';
-
+import '../Screens/signUp.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 final appRouter = GoRouter(
   initialLocation: '/login',
-  redirect: (BuildContext context, GoRouterState state) {
-    final bool isLoggingIn = state.uri.toString() == '/login';
-    final bool isLoggedIn = _auth.currentUser != null;
+  // redirect: (BuildContext context, GoRouterState state) {
+  //   final bool isLoggingIn = state.uri.toString() == '/login';
+  //   final bool isLoggedIn = _auth.currentUser != null;
 
-    // Si el usuario está intentando acceder al login pero ya está autenticado
-    if (isLoggingIn && isLoggedIn) {
-      return '/client'; // redirige al cliente si ya está autenticado
-    }
+  //   // Si el usuario está intentando acceder al login pero ya está autenticado
+  //   if (isLoggingIn && isLoggedIn) {
+  //     return '/client'; // redirige al cliente si ya está autenticado
+  //   }
 
-    // Si el usuario no está autenticado y trata de acceder a rutas que requieren autenticación
-    if (!isLoggedIn && state.uri.toString() != '/login') {
-      return '/login';
-    }
+  //   // Si el usuario no está autenticado y trata de acceder a rutas que requieren autenticación
+  //   if (!isLoggedIn && state.uri.toString() != '/login') {
+  //     return '/login';
+  //   }
 
-    // Si no hay condiciones de redirección, retornar null
-    return null;
-  },
+  //   // Si no hay condiciones de redirección, retornar null
+  //   return null;
+  // },
   routes: [
     GoRoute(
       path: '/login',
@@ -53,6 +53,11 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/admin/product-list',
       builder: (context, state) => ProductListScreen(),
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) =>
+          SignUpScreen(), // Asegúrate de que esta línea exista
     ),
   ],
 );
